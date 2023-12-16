fb7949f6b0ce270f17151031e29f12d68523174c
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-12-17 00:22:21 +0530

    Add files via upload

---
+++ Conditionals and Loops/ifelse.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+   int n;
+   cin>>n;
+   
+   if(n>0){
+       cout<<"Positive"<<endl;
+   }
+   
+   else if(n<0){
+       cout<<"Negative"<<endl;
+   }
+   
+   else{
+       cout<<"Zero"<<endl;
+   }
+   
+    return 0;
+}
