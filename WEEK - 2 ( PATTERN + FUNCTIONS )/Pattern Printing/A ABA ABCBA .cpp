67344fc0c5f1e6792b71e2abf734190c50e63f36
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-12-19 10:29:08 +0530

    Add files via upload

---
+++ WEEK - 2 ( PATTERN + FUNCTIONS )/Pattern Printing/A ABA ABCBA .cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n ;
+    cin>>n;
+    
+    for(int row=0;row<n;row++){
+        char ch;
+        for(int col=0;col<row+1;col++){
+            int number = col +1;
+           ch = number + 'A' - 1;
+           cout<<ch;
+        }
+        
+        for(char alphabet = ch; alphabet>'A';){
+            alphabet=alphabet-1;
+            cout<<alphabet;
+        }
+         cout<<endl;
+    }
+    
+
+}
