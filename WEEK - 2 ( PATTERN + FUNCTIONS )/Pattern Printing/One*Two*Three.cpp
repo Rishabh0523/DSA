bca189009357dbd0912f194b17b63ada6ebea669
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-12-19 10:00:18 +0530

    Add files via upload

---
+++ WEEK - 2 ( PATTERN + FUNCTIONS )/Pattern Printing/One*Two*Three.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n ;
+    cin>>n;
+    
+    for(int row=0; row<n;row++){
+        for(int col=0;col<2*row+1;col++){
+            if(col%2== 1){
+                cout<<"*";
+            }
+            
+            else{
+                cout<<row+1;
+            }
+            
+            
+        }
+        
+        cout<<endl;
+    }
+
+    return 0;
+}
