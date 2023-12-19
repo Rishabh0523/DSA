97e6158524f7799ac8b47c70ffd519c67b72d9ef
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-12-20 00:06:34 +0530

    Add files via upload

---
+++ WEEK - 2 ( PATTERN + FUNCTIONS )/BitWise Operators and Loops/Pre - Post Increment.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+//   int a=5;
+//   cout<<(++a);
+
+// int a =10;
+// cout<<(--a)*10;
+// cout<<endl;
+
+// int b=10;
+// cout<<(b--)*10;
+
+// int r = 10;
+// cout<< ++r;
+// cout<<endl;
+// cout<< r++;
+// cout<<endl;
+// cout<< r;
+
+int n=10;
+cout<<((++n)*(n++));
+
+
+}
