879e606768dab37a971d6886d2259a410d5b74b8
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-12-19 09:08:29 +0530

    Add files via upload

---
+++ WEEK - 2 ( PATTERN + FUNCTIONS )/Pattern Printing/Invertd Hollow Pyramid.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    for(int row=0;row<n;row++){
+        for(int col=0;col<row;col++){
+            cout<<" ";
+        }
+        int totalCol = n-row;
+        for(int col=0;col<totalCol;col++){
+            if(col==0 || col == totalCol-1){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        
+        cout<<endl;
+    }
+}
