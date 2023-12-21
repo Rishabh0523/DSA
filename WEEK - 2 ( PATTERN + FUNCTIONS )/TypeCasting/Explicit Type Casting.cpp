7444cd1947b782aa196a23d84b5db0ad89054eff
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-12-21 07:37:35 +0530

    Add files via upload

---
+++ WEEK - 2 ( PATTERN + FUNCTIONS )/TypeCasting/Explicit Type Casting.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  // float to int
+    
+//  int num1=96;
+//  float num2=3.5;
+ 
+//  float result = num1+ (int)num2;
+ 
+//  cout<<result;
+ 
+   // int to float'
+    
+ float num1=96.5;
+ int num2=3;
+ 
+ float result = num1+ ((float)num2);
+ 
+ cout<<result;
+ 
+ // bool to int
+ 
+//  bool pi = 3.176345;
+// int intpi=(int)pi;
+
+// cout<<intpi;
+ 
+
+}
---
+++ WEEK - 2 ( PATTERN + FUNCTIONS )/TypeCasting/Implicit Casting.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+// Int to float
+
+int num1=10;
+float num2=5.5;
+
+// float result = num1+num2;
+
+// int result = num1+num2;
+
+// cout<<result<<endl;
+
+
+// char to int
+
+ int a=90;
+ char ch=a;
+ 
+ cout<<a;
+
+ // int to char
+
+  int a=96;
+ char ch=a;
+ 
+ cout<<ch;
+ 
+
+
+}
