d93f722b6890e04ab89da2c4907898c0f79b61c2
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-12-22 13:32:12 +0530

    Add files via upload

---
+++ WEEK - 3 ( ARRAYS + TIME & SPACE COMPLEXITY )/Basic Arrays/Addressfind.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a = 5;
+  
+  cout<<"address of a is :"<<&a<<endl;
+  
+  int arr[10];
+  cout<<"Base address of arr is : "<<&arr<<endl;
+  cout<<"Base address of arr is : "<<arr<<endl;
+    return 0;
+}
