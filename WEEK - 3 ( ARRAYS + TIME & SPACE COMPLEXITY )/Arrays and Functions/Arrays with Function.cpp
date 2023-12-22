75db00229dc61b3382784cd4054517dd9cf4f858
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-12-22 18:36:25 +0530

    Add files via upload

---
+++ WEEK - 3 ( ARRAYS + TIME & SPACE COMPLEXITY )/Arrays and Functions/Arrays with Function.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int size){
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    
+    cout<<endl;
+}
+
+int main()
+{
+    int arr[5]={2,4,5,6,7};
+    int size=5;
+
+   printArray(arr,size);
+    
+   
+     
+    return 0 ;
+
+}
