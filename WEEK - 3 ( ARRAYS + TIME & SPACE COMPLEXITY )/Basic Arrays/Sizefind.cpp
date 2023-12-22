781f65d2678b7765fd457fc954275aeeee0ccf95
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-12-22 13:32:48 +0530

    Add files via upload

---
+++ WEEK - 3 ( ARRAYS + TIME & SPACE COMPLEXITY )/Basic Arrays/Sizefind.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a = 5;
+  
+  cout<<"Size of a is "<<sizeof(a)<<endl;
+  
+  int arr[10];
+  
+  cout<<"Size of arr is "<<sizeof(arr)<<endl;
+    return 0;
+}
