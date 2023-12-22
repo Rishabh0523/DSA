22c4a93ddfa9323d4435d8c8ef57b332d036c20b
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-12-22 14:06:40 +0530

    Update Taking input in Arrays.cpp

--- WEEK - 3 ( ARRAYS + TIME & SPACE COMPLEXITY )/Basic Arrays/Taking input in Arrays.cpp
+++ WEEK - 3 ( ARRAYS + TIME & SPACE COMPLEXITY )/Basic Arrays/Taking input in Arrays.cpp
@@ -9,10 +9,15 @@ int main()
     int n=5;
     
     for(int i=0;i<n;i++){
+        cout<<"Enter the value of index : "<<i<<endl;
     cin>>arr[i];
     cout<<endl;
     }
     
+    for(int i=0; i<n;i++){
+        cout<<arr[i];
+      
+    }
      
     return 0 ;
 
