250994e954de665135647f04199c10ac344f6024
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-12-22 14:01:23 +0530

    Add files via upload

---
+++ WEEK - 3 ( ARRAYS + TIME & SPACE COMPLEXITY )/Basic Arrays/Taking input in Arrays.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int arr[5];
+    
+    int n=5;
+    
+    for(int i=0;i<n;i++){
+    cin>>arr[i];
+    cout<<endl;
+    }
+    
+     
+    return 0 ;
+
+}
