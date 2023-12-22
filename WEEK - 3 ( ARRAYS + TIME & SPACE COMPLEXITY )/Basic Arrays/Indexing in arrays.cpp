ae164d0aa9a0e078b639f1df5436d300cc893f35
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-12-22 13:52:55 +0530

    Add files via upload

---
+++ WEEK - 3 ( ARRAYS + TIME & SPACE COMPLEXITY )/Basic Arrays/Indexing in arrays.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int arr[]={1,3,4,5,7};
+    
+    int n=5;
+    
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+    
+    //  cout<<"Indexing of 1 is : "<<arr[1]<<endl;
+    //  cout<<"Indexing of 4 is : "<<arr[7]<<endl;
+     
+    return 0 ;
+
+}
