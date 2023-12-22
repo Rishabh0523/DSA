b17799fd26fa9f9773524b25582d2d5943bd4b56
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-12-22 13:39:03 +0530

    Add files via upload

---
+++ WEEK - 3 ( ARRAYS + TIME & SPACE COMPLEXITY )/Basic Arrays/Initialisation of arrays.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int arr[]={1,3,4,5,7};
+    
+    int brr[5]={2,4,5,6,7};
+    
+    int crr[2]={2,3};
+    
+    int hrr[2]={2,3,4,5,6};
+    
+    return 0 ;
+
+}
