ef2f9616867cfbd7c8b42db24cff5658ba9a36a5
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-12-23 10:18:24 +0530

    Update and rename callbyvalue.cpp to Callbyvalue.cpp

---
+++ WEEK - 3 ( ARRAYS + TIME & SPACE COMPLEXITY )/Call by value and Call by reference/Callbyvalue.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int incrementBy1(int n){
+    n = n + 1;
+    return n;
+}
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    n = incrementBy1(n);
+    cout<<n<<endl;
+   
+    return 0 ;
+
+}
