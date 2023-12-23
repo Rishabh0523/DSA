bd3a1f197dedbf66fcdbcc74d65d05a4a5373ca1
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-12-23 10:18:47 +0530

    Add files via upload

---
+++ WEEK - 3 ( ARRAYS + TIME & SPACE COMPLEXITY )/Call by value and Call by reference/Reference variable.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int n=5;
+    int&k = n;
+    int &c= n;
+    
+    cout<<n<<endl;
+    cout<<k<<endl;
+    cout<<c<<endl;
+   
+    return 0 ;
+
+}
