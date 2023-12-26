309d2906685b45cfddf1b378163592c02504f3e9
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-12-26 10:47:03 +0530

    Add files via upload

---
+++ WEEK - 4 ( SEARCHING AND SORTING )/Binary Search/Find Missing Element in a sorted array.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int findMissingElement(int arr[], int n){
+    int s=0;
+    int e=n-1;
+    int ans = -1;
+    int mid =s+(e-s)/2;
+    
+    while(s<=e){
+        int diff=arr[mid] - mid;
+        
+        if(diff==1){
+            s = mid+1;
+        }
+        
+    else{
+        ans =mid;
+        e=mid-1;
+    }
+    mid=s+(e-s)/2;
+    }
+    return ans +1;
+}
+
+
+int main()
+{
+     int arr[]={1,2,3,4,6,7,8,9};
+     int n=8;
+     
+     cout<<"Missing number is "<<findMissingElement(arr,n);
+
+    return 0;
+}
