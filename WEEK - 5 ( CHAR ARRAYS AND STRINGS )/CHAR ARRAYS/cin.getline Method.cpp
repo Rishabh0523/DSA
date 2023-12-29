c72dbe6b50e8d7a2e1f008d1dcfcdadb3be144ab
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-12-30 01:41:45 +0530

    Add files via upload

---
+++ WEEK - 5 ( CHAR ARRAYS AND STRINGS )/CHAR ARRAYS/cin.getline Method.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch[100];
+    // cin>>ch;
+    
+    cin.getline(ch , 100);
+    
+   cout<<"Printing the values of : "<<ch<<endl;
+   
+   
+   
+    return 0;
+}
